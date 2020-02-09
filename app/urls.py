e89bf18bd47dbaafac09ca09bac623903b1c8ef2
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-09 22:30:07 +0300

    Added all the url paths for the different pages

---
+++ app/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+  url(r'^$', views.index, name= 'index'),
+  url(r'^search/', views.search_images, name='search_results'),
+  url(r'^image/(\d+)', views.get_image, name='image_results'),
+  url(r'^new/image$', views.new_image, name='new-image'),
+  url(r'^accounts/profile/$', views.user_profiles, name='profile'),
+  url(r'^like/(\d+)', views.like_image, name='like_image'),
+
+  ]

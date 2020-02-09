a49d80f5cbd945538d723c69a02368e0a80d601a
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-09 21:46:31 +0300

    Added the necessary imports

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.conf import settings
+from django.templatetags.static import static
+from django.shortcuts import render, redirect, render_to_response, HttpResponseRedirect
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import Image, Comment, Profile
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from .forms import NewImageForm, NewCommentForm, ProfileUpdateForm, RegisterForm
+from django.contrib import messages
+from .email import send_welcome_email

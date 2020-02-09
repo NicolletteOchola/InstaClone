f7ae8a151b9f452d7b0d17c30b06ad8249ff1848
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-09 22:43:40 +0300

    Added a porms.py folder and imported the necessary modules

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from crispy_forms.helper import FormHelper
+from django import forms
+from .models import Image, Comment, Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from registration.forms import RegistrationForm
+
+

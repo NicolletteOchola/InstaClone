08b505cf59d96791146d7de6befb8383c460f282
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-09 22:08:40 +0300

    Added the view function to render the main page and the register form

--- app/views.py
+++ app/views.py
@@ -9,3 +9,41 @@ from django.contrib.auth.decorators import login_required
 from .forms import NewImageForm, NewCommentForm, ProfileUpdateForm, RegisterForm
 from django.contrib import messages
 from .email import send_welcome_email
+
+def index(request):
+  date = dt.date.today()
+  images = Image.get_images()
+  commets = Comment.get_comment()
+
+  current_user = request.user 
+  if request.method == 'POST':
+    form = NewCommentForm(request.POST, auto_id=False)
+    img_id = request.POST['image_id']
+    if form.is_valid():
+      comment = form.save(commit=False)
+      comment.author = current_user
+      image = Image.get_image(img_id)
+      comment.image = image
+      comment.save()
+    return redirect(f'/#{img_id}',)
+  else:
+    form = NewCommentForm(auto_id=False)
+  return render(request, 'index.html', {"date": date, "images":images, "comments":comments, "form": form,})
+
+def register(request):
+  if request.method == 'POST':
+    form = RegisterForm(request.POST)
+    if form.is_valid():
+      name = form.cleaned_data['first_name']
+      email = form.cleaned_data['email']
+      form.save()
+      send_welcome_email(name, email)
+      username = form.cleaned_data.get('username')
+      messages.success(request, f'Account created for {username}')
+    return redirect('/')
+  else:
+    form = RegisterForm()
+  return render(request, 'registration/registration_form.html', {'form':form}) 
+
+ 
+

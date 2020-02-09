cc43ba3995b110913ddb08de2e71966ccecc5c32
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-09 16:44:45 +0300

    Added the Image class and the methods and funcions related to it

--- app/models.py
+++ app/models.py
@@ -30,3 +30,53 @@ class Profile(models.Model):
     verbose_name = 'Profile'
     verbose_name_plural = 'Profiles'
 
+class Image(models.Model):
+  image_file = models.ImageField(upload_to='images/', default='images/default.jpg')
+  image_name = models.CharField(max_length=255)
+  description = models.TextField()
+  pub_date = models.DateTimeField(auto_now_add=True)
+  Author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+  author_profile = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, default='1')
+  likes = models.ManyToManyField(User, related_name= 'likes', blank = True)
+
+  def save_image(self):
+    self.save()
+
+  @classmethod
+  def get_images(cls):
+    images = cls.objects.all()
+    return images
+  
+  @classmethod
+  def search_images(cls, search_term):
+    images = cls.objects.filter(description__icontains=search_term)
+    return images
+
+  @classmethod
+  def get_by_author(cls, Author):
+    images = cls.objects.filter(Author=Author)
+    return images
+
+  def total_likes(self):
+    self.likes.count()
+
+  @classmethod
+  def get_image(request, id):
+    try:
+        image = Image.objects.get(pk = id)
+        print(image)
+    except ObjectDoesNotExist:
+      raise Http404()
+
+    return image
+
+  def __str__(self):
+    return self.image_name
+
+  class Meta:
+    ordering = ['-pub_date']
+    verbose_name = 'My image'
+    verbose_name_plural = 'Images'
+
+    
+

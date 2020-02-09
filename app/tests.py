735a20db110a718da50a603c6e3f8e4d086a8c97
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-09 23:02:37 +0300

    Added a test case for adding an image

--- app/tests.py
+++ app/tests.py
@@ -5,4 +5,12 @@ from django.contrib.auth.models import User
 
 # Create your tests here.
 
+class ImageTestCase(TestCase):
+  def setUp(self):
+    self.image= Image(image_name = 'Beagle', description ='Lovely dog', image_file ='images/beagle.jpg', location = self.new_location)
+    self.image.save_image()
 
+ def test_save_images(self):
+    self.image.save_image()
+    all_images = Image.objects.all()
+    self.assertTrue(len(all_images) > 0)

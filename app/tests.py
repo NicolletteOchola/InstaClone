dc18cf780d99f63e6b06f6680314679133597f6c
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-09 22:58:42 +0300

    Imported the necessary modules for the tests to be defined and run

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,8 @@
+from django.db import models
 from django.test import TestCase
+from .models import Profile, Image, Location
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+
